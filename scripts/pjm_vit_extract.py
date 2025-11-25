ec5363d7936b39912b3d4a8ab7508f758fbb817e
Rostyslav <r.kovalenko.it@gmail.com>
2025-11-25 17:12:39 +0100

    vit extract script

--- scripts/pjm_vit_extract.py
+++ scripts/pjm_vit_extract.py
@@ -1,27 +1,25 @@
 import argparse
-import os
-import os.path as osp
-import glob
 import tqdm
 import torch
 import numpy as np
-import torch.nn.functional as F
-from PIL import Image
 from transformers import AutoImageProcessor, CLIPVisionModel
 import datasets
 import sys
 import lmdb
+from pathlib import Path
+import h5py
+
 sys.path.append('./')
 
 from utils.s2wrapper import forward as multiscale_forward
-from utils.helpers import read_video, get_img_list
 from utils.pjm.preprocessing import get_video_frames, ImageConverter
+from utils.log import get_error_logger
 
 _GLOBAL_SEED = 0
 np.random.seed(_GLOBAL_SEED)
 torch.manual_seed(_GLOBAL_SEED)
 
-
+error_logger = get_error_logger('vit_extract_errors.log')
 
 class ViTFeatureReader(object):
     def __init__(
@@ -71,16 +69,36 @@ def get_parser():
     parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--nth_layer', type=int, default=-1)
     parser.add_argument('--cache_dir', help='cache dir for model', default=None)
+    parser.add_argument('--hdf5-save-interval', help='num of processed videos to flush to hdf5 file (Default: 500)', type=int, default=500)
     
-    parser.add_argument('--save_dir', help='where to save the output', required=True)
+    parser.add_argument('--save_path', help='where to save the output', type=Path, required=True)
     parser.add_argument('--model_name', help='ViT model name', default='openai/clip-vit-large-patch14')
 
     return parser
 
-def get_iterator(args):
+class FeaturesExtractor:
+    def __init__(self, converter: ImageConverter, reader: ViTFeatureReader, batch_size: int):
+        self.converter = converter
+        self.reader = reader
+        self.batch_size = batch_size
+    
+    def extract(self, record: dict[str, any]) -> np.ndarray:
+        frames = get_video_frames(record['mp4'])
+        processed = self.converter.process_frames(frames, record['__key__'])
+        video_features = []
+        for i in range(0, len(processed), self.batch_size):
+            frame_batch = processed[i:i+self.batch_size]
+            frame_features = self.reader.get_feats(frame_batch).cpu().numpy()
+            video_features.append(frame_features)
+            
+        return np.concatenate(video_features, axis=0)
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+
     batch_size = args.batch_size
-    dataset = datasets.load_dataset(args.dataset_path, split='train')
-    lmdb_env = lmdb.open(args.crop_params_path)
+    lmdb_env = lmdb.open(args.crop_params_path, readonly=True, lock=False)
     converter = ImageConverter(lmdb_env, (224,224))
     
     reader = ViTFeatureReader(
@@ -91,43 +109,60 @@ def get_iterator(args):
         nth_layer=args.nth_layer,
         cache_dir=args.cache_dir
     )
+    extractor = FeaturesExtractor(converter, reader, batch_size=batch_size)
+
+    dataset = datasets.load_dataset(args.dataset_path, split='train', cache_dir=args.cache_dir)
     
-    def iterate():
-        for rec in dataset:
-            frames = get_video_frames(rec['mp4'])
-            processed = converter.process_frames(frames, rec['__key__'])
-            frame_features = []
-            for i in range(0, len(processed), batch_size):
-                frame_batch = processed[i:i+batch_size]
-                features = reader.get_feats(frame_batch).cpu().numpy()
-                frame_features.append(features)
-            
-            yield np.concatenate(frame_features, axis=0), rec['__key__']
-            
-    return iterate
-
-
-def main():
-    parser = get_parser()
-    args = parser.parse_args()
-
-    generator = get_iterator(args)
-    iterator = generator()
-
-    for vit_feat in tqdm.tqdm(iterator):
-        feats, id, st = vit_feat
-        save_path = osp.join(args.save_dir, fname, m)
+    output_file: Path = args.save_dir / "vit_feat_pjm.h5"
+    processed_ids = set()
+    if output_file.exists():
+        with h5py.File(output_file, mode='r') as existing:
+            processed_ids = set(existing.keys())
+        dataset = dataset.filter(lambda record: record['__key__'] not in processed_ids)
+    
+    num = len(dataset)
+    num_of_already_processed_videos = len(processed_ids)
+    with h5py.File(name=output_file, mode="a") as hdf5_file:
+        if num_of_already_processed_videos == 0:
+            hdf5_file.attrs["model"] = args.model_name
+            hdf5_file.attrs["overlap_size"] = args.overlap_size
+            hdf5_file.attrs["nth_layer"] = args.nth_layer
+            hdf5_file.attrs["dataset_name"] = "PJM"
+            hdf5_file.attrs["split"] = 'train'
+            hdf5_file.attrs["num"] = num
         
-        postfix = ""
-        if args.s2_mode != "":
-            postfix = f"_{args.s2_mode}"
-        if len(args.scales) == 3:
-            postfix = f'{postfix}_large'
-        if st is not None:
-            postfix = f'_{st}{postfix}'
+        pbar = tqdm.tqdm(total=num, desc='Processing PJM')
+        if num_of_already_processed_videos != 0:
+            pbar.update(num_of_already_processed_videos)
         
-        np.save(osp.join(save_path, f'{id}{postfix}.npy'), feats)
-
+        errors_count = 0
+        processed_count = 0
+        for record in dataset:
+            try:
+                video_features = extractor.extract(record)
+                
+                assert len(video_features.shape) == 2, f"Expected 2D features, got {video_features.shape}"
+                
+                ds = hdf5_file.create_dataset(
+                    record['__key__'],
+                    data=video_features,
+                    dtype='float32'
+                )
+                ds.attrs["num_chunks"] = video_features.shape[0]
+                ds.attrs['features_dim'] = video_features.shape[1]
+                
+                processed_count += 1
+            except Exception:
+                errors_count += 1
+                failed_video_id = record['__key__']
+                error_logger.exception(f"extract features error for: {failed_video_id}")
+            finally:
+                if processed_count % args.hdf5_save_interval == 0:
+                    hdf5_file.flush()
+                pbar.update(1)
+                pbar.set_postfix({'processed': processed_count + num_of_already_processed_videos, 'errors': errors_count})
+        hdf5_file.flush()
+    lmdb_env.close()
 
 if __name__ == "__main__":
     main()
